585776ca4be948569d5ffa575e9cfc7c9973b7a3
Raphaël Fillion <34820473+RaphY18@users.noreply.github.com>
2021-03-24 17:28:45 -0400

    fix btn on discographie not animate

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,16 @@ gsap.registerPlugin(ScrollTrigger);
 let sections = document.querySelectorAll('.section');
 
 sections.forEach(section => {
+    //accueil
    let title = section.querySelectorAll('#title');
    let card = section.querySelectorAll('.card');
    let carousel = section.querySelector('.swiper-container');
    let video = section.querySelector('iframe');
 
+   //discographie
    let list = section.querySelector('#list');
    let img = section.querySelector('#img_disco');
-
+   let btn = section.querySelector('#btn_list')
 
    gsap.timeline({
        scrollTrigger: {
@@ -31,4 +33,6 @@ sections.forEach(section => {
                     {y: 25, opacity: 0}, '-= 0.3')
     .from(img,
                     {x: 25, opacity: 0}, '-= 0.3')
+    .from(btn,
+                    {y: 25, opacity: 0}, '-= 0.3')
 });
